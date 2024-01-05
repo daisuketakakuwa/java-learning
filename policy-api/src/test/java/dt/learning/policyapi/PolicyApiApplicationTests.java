7ddc7a82cecad9cbf596071cd42138c99eff1cff
dtakakuwa129 <daisuke.takakuwa129@gmail.com>
2024-01-05 12:22:16 +0900

    base of policy-api

---
+++ policy-api/src/test/java/dt/learning/policyapi/PolicyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dt.learning.policyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
